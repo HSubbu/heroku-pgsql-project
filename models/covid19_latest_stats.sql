a37be33d6a47c4592d92aa977636bc756e994654
Subramanian Hariharan <subramanianhariharan@Subramanians-MacBook-Air.local>
2022-02-14 18:50:26 -0500

    12th commit

--- models/covid19_latest_stats.sql
+++ models/covid19_latest_stats.sql
@@ -1,4 +1,4 @@
 select c.iso_code, c.total_cases, c.new_cases, c.total_cases_per_million,p.total_population,p.continent 
    from covid19  c
-join population p
+join population_prosperity p
 on c.iso_code = p.code
