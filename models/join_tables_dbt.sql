9609c82c1e2dfd76c7d1463140968c5d916bdab3
Subramanian Hariharan <subramanianhariharan@Subramanians-MacBook-Air.local>
2022-02-14 18:40:30 -0500

    10th commit

--- models/join_tables_dbt.sql
+++
@@ -1,5 +0,0 @@
-select *
-from {{ref('population')}} 
-inner join {{ref('covid19')}} 
-on {{ref('population')}}.code = {{ref('covid19')}}.iso_code 
-where code='{{ var("selected_country") }}' AND year='{{ var("selected_year") }}'
