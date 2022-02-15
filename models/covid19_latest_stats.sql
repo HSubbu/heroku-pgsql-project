a49f385a2b93f60e50f929bddbbfa09212585d98
Subramanian Hariharan <subramanianhari91@gmail.com>
2022-02-15 00:22:15 +0000

    committed from dbt clou

--- models/covid19_latest_stats.sql
+++ models/covid19_latest_stats.sql
@@ -1,4 +1,7 @@
-select c.iso_code, c.total_cases, c.new_cases, c.total_cases_per_million,p.total_population,p.continent 
+select c.iso_code, c.total_cases,p.year, c.new_cases,p.total_population, c.total_cases_per_million,
+       c.population,c.population_density, p.gdp_per_capita
    from covid19  c
 join population_prosperity p
 on c.iso_code = p.code
+where p.year=2019 and
+      (c.iso_code='USA' or c.iso_code='IND')
